934468f2a1a3e8cb24531465f8ce584955f71f19
kelchan <kelvin.chan131@gmail.com>
2022-06-30 10:47:32 -0700

    fixed temperature change js

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ function remove( element ) {
 }
 
 function temp( element ) {
-    console.log( 'working', element.value, document.querySelector( '#change' ).innerText )
+    console.log( 'working', element.value, document.querySelectorAll( '#change' )[0].innerText )
     let selected = element.value;
-    let change = parseInt( document.querySelector( '#change' ).innerText );
-    if( selected == 'F' ) {
-        change = ( change * 9 / 5 ) + 32;
-        document.querySelector( '#change' ).innerHTML = Math.round(change);
-    }
-    if( selected == 'C' ) {
-        change = ( change - 32 ) * 5 / 9;
-        document.querySelector( '#change' ).innerText = Math.round(change);
+    // let num = parseInt( document.querySelector( '#change' ).innerText );
+    for( let i = 0; i < document.querySelectorAll( '#change' ).length; i++ ) {
+        let num = parseInt( document.querySelectorAll( '#change' )[i].innerText );
+        if( selected == 'F' ) {
+            num = ( num * 9 / 5 ) + 32;
+            document.querySelectorAll( '#change' )[i].innerText = Math.round( num );
+        }
+        if( selected == 'C' ) {
+            num = ( num - 32 ) * 5 / 9;
+            document.querySelectorAll( '#change' )[i].innerText = Math.round( num );
+        }
     }
 }
